330b0404d95fe9f718de0bfc837f9e0ab1be4338
Judit Horvath <judit.horvath@appliedtechnology.se>
2024-12-12 09:37:44 +0100

    feat:setup fronend base

--- Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
+++ Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4501abed23117f4c624d93fc75bf30a61b220c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b57806f4a738adcdaf4f476a26a624f35bd4aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
