24905bab569b4ce123721a6a6e63e6c05ad25b59
Judit Horvath <judit.horvath@appliedtechnology.se>
2024-12-12 10:15:09 +0100

    feat:connect frontend with  backend

--- Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
+++ Product.Api/obj/Debug/net8.0/Product.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b57806f4a738adcdaf4f476a26a624f35bd4aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330b0404d95fe9f718de0bfc837f9e0ab1be4338")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
